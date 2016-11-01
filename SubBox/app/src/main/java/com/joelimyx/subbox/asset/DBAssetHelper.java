0abf18793b0384f0c4e87905cfea38db43384c5d
Joe Lim <joelimyx@gmail.com>
2016-10-31 21:49:52 -0400

    setup asset helper

---
+++ SubBox/app/src/main/java/com/joelimyx/subbox/asset/DBAssetHelper.java
@@ -0,0 +1,19 @@
+package com.joelimyx.subbox.asset;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * Created by Joe on 10/31/16.
+ */
+
+public class DBAssetHelper extends SQLiteAssetHelper {
+    public static final String DATABASE_NAME = "subbox.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DBAssetHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
