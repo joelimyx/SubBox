1638d197600bb2a6426497ef0f89e08ad168579c
Joe Lim <joelimyx@gmail.com>
2016-11-16 16:43:48 -0500

    Finish detail activity

--- SubBox/app/src/main/java/com/joelimyx/subbox/Detail/DetailScrollingActivity.java
+++ SubBox/app/src/main/java/com/joelimyx/subbox/Detail/DetailScrollingActivity.java
@@ -1,6 +1,7 @@
 package com.joelimyx.subbox.detail;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 
 import com.joelimyx.subbox.Classes.SubBox;
 import com.joelimyx.subbox.checkout.CheckOutActivity;
+import com.joelimyx.subbox.mainlist.MainActivity;
 import com.joelimyx.subbox.mainlist.SubBoxAdapter;
 import com.joelimyx.subbox.R;
 import com.joelimyx.subbox.dbassethelper.SubBoxHelper;
@@ -39,13 +41,25 @@ public class DetailScrollingActivity extends AppCompatActivity {
         //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
+        AsyncTask<Integer,Void,String> task = new AsyncTask<Integer, Void, String>() {
+            @Override
+            protected String doInBackground(Integer... integers) {
+                return SubBoxHelper.getsInstance(DetailScrollingActivity.this).getSubBoxByID(integers[0]).getImgUrl();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+
+                Picasso.with(DetailScrollingActivity.this)
+                        .load(s)
+                        .fit()
+                        .into(mToolbarImage);
+            }
+        };
+        task.execute(id);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
-        Picasso.with(this)
-                .load(SubBoxHelper.getsInstance(this).getSubBoxByID(id).getImgUrl())
-                .fit()
-                .into(mToolbarImage);
-
         DetailFragment detailFragment = DetailFragment.newInstance(id);
         getSupportFragmentManager().beginTransaction().replace(R.id.detail_fragment_container,detailFragment).commit();
     }
